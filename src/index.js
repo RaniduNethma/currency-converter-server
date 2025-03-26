59b00af21104c4b7b64049958104169f524f360d
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-03-26 21:57:49 +0530

    make middleware and port

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-console.log("hello World!");
-console.log("code testing");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//listen to a port
+app.listen(9000 , () => {
+    console.log("hello")
+})
