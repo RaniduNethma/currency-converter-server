99c46cc453286f60d642fa80afc79a089ced66de
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-03-29 03:35:35 +0530

    api

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/getAllCurrency",async (req, res) => {
+    const nameURL = `https://openexchangerates.org/api/currencies.json?app_id=fb552cb116fb4ec8a530b81c81ee5083`
+    try{
+        const namesResponce = await axios.get(nameURL);
+        const nameData = namesResponce.data;
+
+        return res.json({nameData});
+    }
+    catch(err){
+        console.error(err);
+    }
+})
+
 //listen to a port
 app.listen(9000 , () => {
-    console.log("hello")
+    console.log("hello!")
 })
